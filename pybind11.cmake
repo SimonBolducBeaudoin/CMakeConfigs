888b36ff570b513b9ad6be4e2bc9f91a0ea09533
Simon Bolduc Beaudoin <Simon.Bolduc.Beaudoin@usherbrooke.ca>
2023-11-08 10:34:57 -0500

    Adding MSVC

--- pybind11.cmake
+++ pybind11.cmake
@@ -1,6 +1,7 @@
 # Finding using python to automatically find pybind11 and its dependencies (python dlls)
 string(FIND ${CMAKE_CXX_COMPILER} x86_64-w64-mingw32-g++ IS_MINGW) 
-if(IS_MINGW) #Using anaconda's python for windows
+if(NOT(IS_MINGW EQUAL -1)) 
+    #Using anaconda's python for windows
     set(CONDA_PY_EXE "C:/Anaconda2/python")
     set(CONDA_PY_LINKS "-LC:/Anaconda2/ -lpython27")
     if(CYGWIN)
@@ -9,7 +10,12 @@ if(IS_MINGW) #Using anaconda's python for windows
     endif()
     set(Python_EXECUTABLE ${CONDA_PY_EXE})
     list(APPEND LINKS ${CONDA_PY_LINKS})
-else() #unix
+elseif(MSVC)
+    find_package(Python COMPONENTS Interpreter REQUIRED)
+    set(Python_EXECUTABLE "C:/Anaconda2/python")
+    find_library(PYLIB NAMES python27 HINTS "C:/Anaconda2/libs")
+    list(APPEND LINKS ${PYLIB})
+else() #unix 
     find_package(Python COMPONENTS Interpreter REQUIRED)
 endif()
 
@@ -24,6 +30,9 @@ separate_arguments(PY_INCL)
 
 # shared labrary convention
 if ( IS_MINGW ) # genrate libbrary named "name.pyd" else it's named "libname.so"
+    set(SHARED_LIB_PREFIX "")
+    set(SHARED_LIB_SUFFIX ".pyd")
+elseif(MSVC)
     set(SHARED_LIB_PREFIX "")
     set(SHARED_LIB_SUFFIX ".pyd")
 else () #unix
