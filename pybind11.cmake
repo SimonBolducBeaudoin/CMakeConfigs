ab10ae1fb30f66413104b659f739f6c2512a15f3
Simon Bolduc Beaudoin <simonbolducbeaudoin@gmail.com>
2024-04-23 16:07:09 -0400

    Flexible python search
    pybind11.cmale now looks in the system path for python (prefering anaconda installations) and detect python version number to use the right dll.

--- pybind11.cmake
+++ pybind11.cmake
@@ -1,15 +1,25 @@
 # Finding using python to automatically find pybind11 and its dependencies (python dlls)
 string(FIND ${CMAKE_CXX_COMPILER} x86_64-w64-mingw32-g++ IS_MINGW) 
+
 if(IS_MINGW) #Using anaconda's python for windows
-    set(CONDA_PY_EXE "C:/Anaconda2/python")
-    set(CONDA_PY_LINKS "-LC:/Anaconda2/ -lpython27")
+	message(STATUS "Cross-compiling using MINGW")
+	# This line is searching for the Python executable. It first looks in "C:/Anaconda3" and "C:/Anaconda2".
+	# If Python is not found in these locations, find_program will proceed to search in the default system paths.
+	message(STATUS "Looking for python installation ...")
+	find_program(ANACONDA_PYTHON python PATHS "C:/Anaconda3" "C:/Anaconda2" ) 
+	message(STATUS "Found python at : ${ANACONDA_PYTHON}")
+	get_filename_component(CONDA_PY_DIR ${ANACONDA_PYTHON} DIRECTORY)
+    set(CONDA_PY_EXE ${ANACONDA_PYTHON})
+    set(CONDA_PY_LINKS "-L${CONDA_PY_DIR} -lpython27")
     if(CYGWIN)
+		message(STATUS "Cygwin environnment detected.")
         string(REGEX REPLACE "C\:" "/cygdrive/c" CONDA_PY_EXE ${CONDA_PY_EXE})
         string(REGEX REPLACE "C\:" "/cygdrive/c" CONDA_PY_LINKS ${CONDA_PY_LINKS})
     endif()
     set(Python_EXECUTABLE ${CONDA_PY_EXE})
     list(APPEND LINKS ${CONDA_PY_LINKS})
 else() #unix
+	message(STATUS "Compiling for a unix system.")
     find_package(Python COMPONENTS Interpreter REQUIRED)
 endif()
 
