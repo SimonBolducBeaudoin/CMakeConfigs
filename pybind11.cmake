de486c5aaa22525665e533663019ebd4e47b1610
Simon Bolduc Beaudoin <simonbolducbeaudoin@gmail.com>
2024-04-23 17:06:14 -0400

    Flexible python search

--- pybind11.cmake
+++ pybind11.cmake
@@ -1,21 +1,37 @@
-# Finding using python to automatically find pybind11 and its dependencies (python dlls)
+	      # Finding using python to automatically find pybind11 and its dependencies (python dlls)
 string(FIND ${CMAKE_CXX_COMPILER} x86_64-w64-mingw32-g++ IS_MINGW) 
 
 if(IS_MINGW) #Using anaconda's python for windows
 	message(STATUS "Cross-compiling using MINGW")
+	#################################################################################
 	# This line is searching for the Python executable. It first looks in "C:/Anaconda3" and "C:/Anaconda2".
 	# If Python is not found in these locations, find_program will proceed to search in the default system paths.
 	message(STATUS "Looking for python installation ...")
-	find_program(ANACONDA_PYTHON python PATHS "C:/Anaconda3" "C:/Anaconda2" ) 
+	find_program(ANACONDA_PYTHON python HINTS "C:/Anaconda3" "C:/Anaconda2" "C:/tools/Anaconda3") 
 	message(STATUS "Found python at : ${ANACONDA_PYTHON}")
+	#################################################################################
+	
+	#################################################################################
+	# Find python version number
+	execute_process(COMMAND ${ANACONDA_PYTHON} -c "import sys; print('.'.join(map(str, sys.version_info[:2])))"
+					OUTPUT_VARIABLE PYTHON_VERSION
+					OUTPUT_STRIP_TRAILING_WHITESPACE)
+	string(REPLACE "." "" PYTHON_VERSION_NO_DOTS ${PYTHON_VERSION})
+	message(STATUS "Python version found : ${PYTHON_VERSION}")
+	#################################################################################
+	
 	get_filename_component(CONDA_PY_DIR ${ANACONDA_PYTHON} DIRECTORY)
+	
     set(CONDA_PY_EXE ${ANACONDA_PYTHON})
-    set(CONDA_PY_LINKS "-L${CONDA_PY_DIR} -lpython27")
-    if(CYGWIN)
-		message(STATUS "Cygwin environnment detected.")
-        string(REGEX REPLACE "C\:" "/cygdrive/c" CONDA_PY_EXE ${CONDA_PY_EXE})
-        string(REGEX REPLACE "C\:" "/cygdrive/c" CONDA_PY_LINKS ${CONDA_PY_LINKS})
-    endif()
+	set(CONDA_PY_LINKS "-L${CONDA_PY_DIR} -lpython${PYTHON_VERSION_NO_DOTS}")
+    #set(CONDA_PY_LINKS "-L${CONDA_PY_DIR} -lpython27")
+    # Deprecated ####################################################################
+	# if(CYGWIN)
+		# message(STATUS "Cygwin environnment detected.")
+        # string(REGEX REPLACE "C\:" "/cygdrive/c" CONDA_PY_EXE ${CONDA_PY_EXE})
+        # string(REGEX REPLACE "C\:" "/cygdrive/c" CONDA_PY_LINKS ${CONDA_PY_LINKS})
+    # endif()
+	#################################################################################
     set(Python_EXECUTABLE ${CONDA_PY_EXE})
     list(APPEND LINKS ${CONDA_PY_LINKS})
 else() #unix
